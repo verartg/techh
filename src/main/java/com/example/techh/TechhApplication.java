5141a0bbcd749ebc257b53857602df75a4a496a4
verartg <verarottger@gmail.com>
2023-05-03 16:24:27 +0200

    test

---
+++ src/main/java/com/example/techh/TechhApplication.java
@@ -0,0 +1,13 @@
+package com.example.techh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/techh/TechhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
